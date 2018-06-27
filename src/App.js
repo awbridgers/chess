be26bfd7d9a99ff3f326cabce6c82e1dddc3d565
awbridgers <awbridgers@users.noreply.github.com>
2018-06-27 10:10:52 -0400

    created board

--- src/App.js
+++ src/App.js
@@ -1,19 +1,57 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Chess from './chess.jsx'
+
+const buildArray = () => {
+  const innerArray = [1,2,3,4,5,6,7,8]
+  const outerArray = []
+  for(let i = 0; i < 8; i++){
+    outerArray.push(innerArray);
+  }
+  return outerArray;
+}
+
+
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {board: buildArray()}
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <div  className = 'board'>
+          <table className = 'table'>
+            <tbody>
+              {this.state.board.map((x,i)=>{
+                return(
+                  <tr key = {i} className = {"row" + i}>
+                    {x.map((y,j) => {
+                      if((i % 2 === 0 && j % 2 !=0) || (i % 2 != 0 && j % 2 === 0)){
+                        return (
+                          <td key ={j} className = {'dark-square'}>
+
+                          </td>
+                        )
+                      }
+                      else{
+                        return (
+                          <td key ={j} className = {'light-square'}>
+                            Q
+                          </td>
+                        )
+                      }
+                    })}
+                  </tr>
+                )
+              })}
+          </tbody>
+        </table>
+        </div>
       </div>
+
     );
   }
 }
