5303c6961725b2b7719e57bc44a1c309f1e7b016
awbridgers <awbridgers@users.noreply.github.com>
2018-06-28 18:05:30 -0400

    adds ability to move pieces

--- src/App.js
+++ src/App.js
@@ -4,13 +4,43 @@ import './App.css';
 import Chess from 'chess.js'
 import Board from './board.jsx'
 
-const buildArray = () => {
-  const innerArray = [1,2,3,4,5,6,7,8]
-  const outerArray = []
-  for(let i = 0; i < 8; i++){
-    outerArray.push(innerArray);
-  }
-  return outerArray;
+//this function takes the possible moves array and strips the elements to only contain a coordinate
+const returnSquare = (array, target) => {
+  let returnArray =[];
+  array.forEach((square) =>{
+    let temp = "";
+    let newString = ""
+    //if the last character denotes check or checkmate, cut it off
+    if(square[square.length-1] === '#' || square[square.length-1] === '+'){
+      newString = square.slice(0,-1)
+    }
+    else{
+      newString = square;
+    }
+    //handle castling
+    if(newString === 'O-O'){
+      if(target === 'e1'){
+        temp = "g1"
+      }
+      else{
+        temp = "g8"
+      }
+    }
+    else if(newString === 'O-O-O'){
+      if(target === 'e1'){
+        temp = "c1"
+      }
+      else{
+        temp = "c8"
+      }
+    }
+    else{
+      temp = newString.substr(newString.length - 2)
+    }
+
+    returnArray.push(temp)
+  })
+  return returnArray;
 }
 
 
@@ -18,19 +48,79 @@ const buildArray = () => {
 class App extends Component {
   constructor(){
     super();
-    this.state = {board: buildArray()}
+    this.state = {target: "", fen: 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1'}
     this.chess = new Chess();
-    this.clickSquare = this.clickSquare.bind(this)
-  }
-  clickSquare(e){
-    let possibleMoves = this.chess.moves({square: e.target.id})
-    console.log(possibleMoves)
+    this.clickSquare = this.clickSquare.bind(this);
+    this.chooseTarget = this.chooseTarget.bind(this);
+    this.possibleMoves =[];
+    this.firstClick = false;
+    this.secondClick = false;
+    this.turn = 'player';
 
+  }
+  componentDidMount(){
+    this.chess.load(this.state.fen)
+  }
+  componentDidUpdate(){
+    if(this.turn === 'computer'){
+      console.log("computer turn")
+    }
+  }
+  chooseTarget(targeted){
+  //if the target is equal to the clicked on square, outline in red
+  if(targeted === this.state.target){
+    return 'targeted'
+  }
+  else{
+    //if its not the clicked on square, see if its a possible move square
+    for(let i=0; i < this.possibleMoves.length; i++){
+      //if possible move, turn yellow.
+      if(targeted === this.possibleMoves[i].substr(this.possibleMoves[i].length -2)){
+        // console.log(this.possibleMoves[i])
+        return 'possibleMove'
+      }
+    }
+    //if it has nothing special about it, stay normal
+    return 'normal'
+  }
+}
+  clickSquare(e){
+    //first click
+    if(this.turn === "player"){
+      if(!this.firstClick){
+        //find all the possible moves
+        this.possibleMoves = returnSquare(this.chess.moves({square: e.target.id}), e.target.id);
+        //if the piece can move
+        if(this.possibleMoves.length > 0){
+          this.firstClick = true;
+          console.log(this.possibleMoves);
+          //set the clicked on square to state.target
+          this.setState({target: e.target.id})
+        }
+      }
+        //second click
+      else{
+        //if the click is on a possible move square, move the first click piece to second click location
+        let location = e.target.id;
+        for(let i=0; i < this.possibleMoves.length; i++){
+          if(location === this.possibleMoves[i].substr(this.possibleMoves[i].length -2)){
+            this.chess.move({from: this.state.target, to: location});
+            this.turn = 'computer';
+          }
+        }
+
+        this.possibleMoves =[];
+        this.firstClick = false;
+        this.setState({fen: this.chess.fen(), target:""})
+
+
+      }
+    }
   }
   render() {
     return (
-      <div>
-        <Board onClick = {this.clickSquare}/>
+      <div className = 'App'>
+        <Board onClick = {this.clickSquare} chooseClass = {this.chooseTarget} fen = {this.state.fen}/>
       </div>
 
     );
--- src/board.jsx
+++ src/board.jsx
@@ -8,8 +8,14 @@ export default class Board extends Component{
   constructor(){
     super();
     this.readFEN = this.readFEN.bind(this);
-    this.state = {fen: "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 1 2", pieceArray: [], highlighted: 'none'}
-    this.showPossibleMoves = this.showPossibleMoves.bind(this);
+    this.state = {fen: "rnbqkbnr/p1ppppp1/7p/1pP5/8/8/PP1PPPPP/RNBQKBNR w KQkq b6 0 3", pieceArray: [], target: "", moves:[]}
+    this.highlight = this.highlight.bind(this);
+  }
+  componentDidUpdate(prevProps){
+    if(this.props.fen !== prevProps.fen){
+      this.readFEN();
+    }
+
   }
   componentDidMount(){
     this.readFEN();
@@ -17,16 +23,16 @@ export default class Board extends Component{
   readFEN(){
     let fenArray = [];
     let piecePlacement =[];
-
+    console.log(this.props.fen)
     let temp = "";
     //separate the FEN by row and info
-    for(let i=0;  i < this.state.fen.length; i++){
-      if(this.state.fen[i] === "/" || this.state.fen[i] === " "){
+    for(let i=0;  i < this.props.fen.length; i++){
+      if(this.props.fen[i] === "/" || this.props.fen[i] === " "){
         fenArray.push(temp);
         temp = "";
       }
       else{
-        temp += this.state.fen[i];
+        temp += this.props.fen[i];
       }
     }
     //once we have an array of each row, interpret the FEN
@@ -50,9 +56,12 @@ export default class Board extends Component{
     this.setState({pieceArray: piecePlacement})
 
   }
-  showPossibleMoves(e){
-
+  highlight(e){
+    console.log(e.target.className)
+    this.setState({target:e.target.id})
+    console.log(this.state.target)
   }
+
 
   render(){
     return(
@@ -60,84 +69,84 @@ export default class Board extends Component{
         <table className = 'table'>
           <tbody>
             <tr>
-              <td id = 'a8' onClick = {this.props.onClick}>{this.state.pieceArray[0]}</td>
-              <td id = 'b8' onClick = {this.props.onClick}>{this.state.pieceArray[1]}</td>
-              <td id = 'c8' onClick = {this.props.onClick}>{this.state.pieceArray[2]}</td>
-              <td id = 'd8' onClick = {this.props.onClick}>{this.state.pieceArray[3]}</td>
-              <td id = 'e8' onClick = {this.props.onClick}>{this.state.pieceArray[4]}</td>
-              <td id = 'f8' onClick = {this.props.onClick}>{this.state.pieceArray[5]}</td>
-              <td id = 'g8' onClick = {this.props.onClick}>{this.state.pieceArray[6]}</td>
-              <td id = 'h8' onClick = {this.props.onClick}>{this.state.pieceArray[7]}</td>
+              <td id = 'a8' className = {this.props.chooseClass('a8')} onClick = {this.props.onClick}>{this.state.pieceArray[0]}</td>
+              <td id = 'b8' className = {this.props.chooseClass('b8')} onClick = {this.props.onClick}>{this.state.pieceArray[1]}</td>
+              <td id = 'c8' className = {this.props.chooseClass('c8')} onClick = {this.props.onClick}>{this.state.pieceArray[2]}</td>
+              <td id = 'd8' className = {this.props.chooseClass('d8')} onClick = {this.props.onClick}>{this.state.pieceArray[3]}</td>
+              <td id = 'e8' className = {this.props.chooseClass('e8')} onClick = {this.props.onClick}>{this.state.pieceArray[4]}</td>
+              <td id = 'f8' className = {this.props.chooseClass('f8')} onClick = {this.props.onClick}>{this.state.pieceArray[5]}</td>
+              <td id = 'g8' className = {this.props.chooseClass('g8')} onClick = {this.props.onClick}>{this.state.pieceArray[6]}</td>
+              <td id = 'h8' className = {this.props.chooseClass('h8')} onClick = {this.props.onClick}>{this.state.pieceArray[7]}</td>
             </tr>
             <tr>
-              <td id = 'a7' onClick = {this.props.onClick}>{this.state.pieceArray[8]}</td>
-              <td id = 'b7' onClick = {this.props.onClick}>{this.state.pieceArray[9]}</td>
-              <td id = 'c7' onClick = {this.props.onClick}>{this.state.pieceArray[10]}</td>
-              <td id = 'd7' onClick = {this.props.onClick}>{this.state.pieceArray[11]}</td>
-              <td id = 'e7' onClick = {this.props.onClick}>{this.state.pieceArray[12]}</td>
-              <td id = 'f7' onClick = {this.props.onClick}>{this.state.pieceArray[13]}</td>
-              <td id = 'g7' onClick = {this.props.onClick}>{this.state.pieceArray[14]}</td>
-              <td id = 'h7' onClick = {this.props.onClick}>{this.state.pieceArray[15]}</td>
+              <td id = 'a7' className = {this.props.chooseClass('a7')} onClick = {this.props.onClick}>{this.state.pieceArray[8]}</td>
+              <td id = 'b7' className = {this.props.chooseClass('b7')} onClick = {this.props.onClick}>{this.state.pieceArray[9]}</td>
+              <td id = 'c7' className = {this.props.chooseClass('c7')} onClick = {this.props.onClick}>{this.state.pieceArray[10]}</td>
+              <td id = 'd7' className = {this.props.chooseClass('d7')} onClick = {this.props.onClick}>{this.state.pieceArray[11]}</td>
+              <td id = 'e7' className = {this.props.chooseClass('e7')} onClick = {this.props.onClick}>{this.state.pieceArray[12]}</td>
+              <td id = 'f7' className = {this.props.chooseClass('f7')} onClick = {this.props.onClick}>{this.state.pieceArray[13]}</td>
+              <td id = 'g7' className = {this.props.chooseClass('g7')} onClick = {this.props.onClick}>{this.state.pieceArray[14]}</td>
+              <td id = 'h7' className = {this.props.chooseClass('h7')} onClick = {this.props.onClick}>{this.state.pieceArray[15]}</td>
             </tr>
             <tr>
-              <td id = 'a6' onClick = {this.props.onClick}>{this.state.pieceArray[16]}</td>
-              <td id = 'b6' onClick = {this.props.onClick}>{this.state.pieceArray[17]}</td>
-              <td id = 'c6' onClick = {this.props.onClick}>{this.state.pieceArray[18]}</td>
-              <td id = 'd6' onClick = {this.props.onClick}>{this.state.pieceArray[19]}</td>
-              <td id = 'e6' onClick = {this.props.onClick}>{this.state.pieceArray[20]}</td>
-              <td id = 'f6' onClick = {this.props.onClick}>{this.state.pieceArray[21]}</td>
-              <td id = 'g6' onClick = {this.props.onClick}>{this.state.pieceArray[22]}</td>
-              <td id = 'h6' onClick = {this.props.onClick}>{this.state.pieceArray[23]}</td>
+              <td id = 'a6' className = {this.props.chooseClass('a6')} onClick = {this.props.onClick}>{this.state.pieceArray[16]}</td>
+              <td id = 'b6' className = {this.props.chooseClass('b6')} onClick = {this.props.onClick}>{this.state.pieceArray[17]}</td>
+              <td id = 'c6' className = {this.props.chooseClass('c6')} onClick = {this.props.onClick}>{this.state.pieceArray[18]}</td>
+              <td id = 'd6' className = {this.props.chooseClass('d6')} onClick = {this.props.onClick}>{this.state.pieceArray[19]}</td>
+              <td id = 'e6' className = {this.props.chooseClass('e6')} onClick = {this.props.onClick}>{this.state.pieceArray[20]}</td>
+              <td id = 'f6' className = {this.props.chooseClass('f6')} onClick = {this.props.onClick}>{this.state.pieceArray[21]}</td>
+              <td id = 'g6' className = {this.props.chooseClass('g6')} onClick = {this.props.onClick}>{this.state.pieceArray[22]}</td>
+              <td id = 'h6' className = {this.props.chooseClass('h6')} onClick = {this.props.onClick}>{this.state.pieceArray[23]}</td>
             </tr>
             <tr>
-              <td id = 'a5' onClick = {this.props.onClick}>{this.state.pieceArray[24]}</td>
-              <td id = 'b5' onClick = {this.props.onClick}>{this.state.pieceArray[25]}</td>
-              <td id = 'c5' onClick = {this.props.onClick}>{this.state.pieceArray[26]}</td>
-              <td id = 'd5' onClick = {this.props.onClick}>{this.state.pieceArray[27]}</td>
-              <td id = 'e5' onClick = {this.props.onClick}>{this.state.pieceArray[28]}</td>
-              <td id = 'f5' onClick = {this.props.onClick}>{this.state.pieceArray[29]}</td>
-              <td id = 'g5' onClick = {this.props.onClick}>{this.state.pieceArray[30]}</td>
-              <td id = 'h5' onClick = {this.props.onClick}>{this.state.pieceArray[31]}</td>
+              <td id = 'a5' className = {this.props.chooseClass('a5')} onClick = {this.props.onClick}>{this.state.pieceArray[24]}</td>
+              <td id = 'b5' className = {this.props.chooseClass('b5')} onClick = {this.props.onClick}>{this.state.pieceArray[25]}</td>
+              <td id = 'c5' className = {this.props.chooseClass('c5')} onClick = {this.props.onClick}>{this.state.pieceArray[26]}</td>
+              <td id = 'd5' className = {this.props.chooseClass('d5')} onClick = {this.props.onClick}>{this.state.pieceArray[27]}</td>
+              <td id = 'e5' className = {this.props.chooseClass('e5')} onClick = {this.props.onClick}>{this.state.pieceArray[28]}</td>
+              <td id = 'f5' className = {this.props.chooseClass('f5')} onClick = {this.props.onClick}>{this.state.pieceArray[29]}</td>
+              <td id = 'g5' className = {this.props.chooseClass('g5')} onClick = {this.props.onClick}>{this.state.pieceArray[30]}</td>
+              <td id = 'h5' className = {this.props.chooseClass('h5')} onClick = {this.props.onClick}>{this.state.pieceArray[31]}</td>
             </tr>
             <tr>
-              <td id = 'a4' onClick = {this.props.onClick}>{this.state.pieceArray[32]}</td>
-              <td id = 'b4' onClick = {this.props.onClick}>{this.state.pieceArray[33]}</td>
-              <td id = 'c4' onClick = {this.props.onClick}>{this.state.pieceArray[34]}</td>
-              <td id = 'd4' onClick = {this.props.onClick}>{this.state.pieceArray[35]}</td>
-              <td id = 'e4' onClick = {this.props.onClick}>{this.state.pieceArray[36]}</td>
-              <td id = 'f4' onClick = {this.props.onClick}>{this.state.pieceArray[37]}</td>
-              <td id = 'g4' onClick = {this.props.onClick}>{this.state.pieceArray[38]}</td>
-              <td id = 'h4' onClick = {this.props.onClick}>{this.state.pieceArray[39]}</td>
+              <td id = 'a4' className = {this.props.chooseClass('a4')} onClick = {this.props.onClick}>{this.state.pieceArray[32]}</td>
+              <td id = 'b4' className = {this.props.chooseClass('b4')} onClick = {this.props.onClick}>{this.state.pieceArray[33]}</td>
+              <td id = 'c4' className = {this.props.chooseClass('c4')} onClick = {this.props.onClick}>{this.state.pieceArray[34]}</td>
+              <td id = 'd4' className = {this.props.chooseClass('d4')} onClick = {this.props.onClick}>{this.state.pieceArray[35]}</td>
+              <td id = 'e4' className = {this.props.chooseClass('e4')} onClick = {this.props.onClick}>{this.state.pieceArray[36]}</td>
+              <td id = 'f4' className = {this.props.chooseClass('f4')} onClick = {this.props.onClick}>{this.state.pieceArray[37]}</td>
+              <td id = 'g4' className = {this.props.chooseClass('g4')} onClick = {this.props.onClick}>{this.state.pieceArray[38]}</td>
+              <td id = 'h4' className = {this.props.chooseClass('h4')} onClick = {this.props.onClick}>{this.state.pieceArray[39]}</td>
             </tr>
             <tr>
-              <td id = 'a3' onClick = {this.props.onClick}>{this.state.pieceArray[40]}</td>
-              <td id = 'b3' onClick = {this.props.onClick}>{this.state.pieceArray[41]}</td>
-              <td id = 'c3' onClick = {this.props.onClick}>{this.state.pieceArray[42]}</td>
-              <td id = 'd3' onClick = {this.props.onClick}>{this.state.pieceArray[43]}</td>
-              <td id = 'e3' onClick = {this.props.onClick}>{this.state.pieceArray[44]}</td>
-              <td id = 'f3' onClick = {this.props.onClick}>{this.state.pieceArray[45]}</td>
-              <td id = 'g3' onClick = {this.props.onClick}>{this.state.pieceArray[46]}</td>
-              <td id = 'h3' onClick = {this.props.onClick}>{this.state.pieceArray[47]}</td>
+              <td id = 'a3' className = {this.props.chooseClass('a3')} onClick = {this.props.onClick}>{this.state.pieceArray[40]}</td>
+              <td id = 'b3' className = {this.props.chooseClass('b3')} onClick = {this.props.onClick}>{this.state.pieceArray[41]}</td>
+              <td id = 'c3' className = {this.props.chooseClass('c3')} onClick = {this.props.onClick}>{this.state.pieceArray[42]}</td>
+              <td id = 'd3' className = {this.props.chooseClass('d3')} onClick = {this.props.onClick}>{this.state.pieceArray[43]}</td>
+              <td id = 'e3' className = {this.props.chooseClass('e3')} onClick = {this.props.onClick}>{this.state.pieceArray[44]}</td>
+              <td id = 'f3' className = {this.props.chooseClass('f3')} onClick = {this.props.onClick}>{this.state.pieceArray[45]}</td>
+              <td id = 'g3' className = {this.props.chooseClass('g3')} onClick = {this.props.onClick}>{this.state.pieceArray[46]}</td>
+              <td id = 'h3' className = {this.props.chooseClass('h3')} onClick = {this.props.onClick}>{this.state.pieceArray[47]}</td>
             </tr>
             <tr>
-              <td id = 'a2' onClick = {this.props.onClick}>{this.state.pieceArray[48]}</td>
-              <td id = 'b2' onClick = {this.props.onClick}>{this.state.pieceArray[49]}</td>
-              <td id = 'c2' onClick = {this.props.onClick}>{this.state.pieceArray[50]}</td>
-              <td id = 'd2' onClick = {this.props.onClick}>{this.state.pieceArray[51]}</td>
-              <td id = 'e2' onClick = {this.props.onClick}>{this.state.pieceArray[52]}</td>
-              <td id = 'f2' onClick = {this.props.onClick}>{this.state.pieceArray[53]}</td>
-              <td id = 'g2' onClick = {this.props.onClick}>{this.state.pieceArray[54]}</td>
-              <td id = 'h2' onClick = {this.props.onClick}>{this.state.pieceArray[55]}</td>
+              <td id = 'a2' className = {this.props.chooseClass('a2')} onClick = {this.props.onClick}>{this.state.pieceArray[48]}</td>
+              <td id = 'b2' className = {this.props.chooseClass('b2')} onClick = {this.props.onClick}>{this.state.pieceArray[49]}</td>
+              <td id = 'c2' className = {this.props.chooseClass('c2')} onClick = {this.props.onClick}>{this.state.pieceArray[50]}</td>
+              <td id = 'd2' className = {this.props.chooseClass('d2')} onClick = {this.props.onClick}>{this.state.pieceArray[51]}</td>
+              <td id = 'e2' className = {this.props.chooseClass('e2')} onClick = {this.props.onClick}>{this.state.pieceArray[52]}</td>
+              <td id = 'f2' className = {this.props.chooseClass('f2')} onClick = {this.props.onClick}>{this.state.pieceArray[53]}</td>
+              <td id = 'g2' className = {this.props.chooseClass('g2')} onClick = {this.props.onClick}>{this.state.pieceArray[54]}</td>
+              <td id = 'h2' className = {this.props.chooseClass('h2')} onClick = {this.props.onClick}>{this.state.pieceArray[55]}</td>
             </tr>
             <tr>
-              <td id = 'a1' onClick = {this.props.onClick}>{this.state.pieceArray[56]}</td>
-              <td id = 'b1' onClick = {this.props.onClick}>{this.state.pieceArray[57]}</td>
-              <td id = 'c1' onClick = {this.props.onClick}>{this.state.pieceArray[58]}</td>
-              <td id = 'd1' onClick = {this.props.onClick}>{this.state.pieceArray[59]}</td>
-              <td id = 'e1' onClick = {this.props.onClick}>{this.state.pieceArray[60]}</td>
-              <td id = 'f1' onClick = {this.props.onClick}>{this.state.pieceArray[61]}</td>
-              <td id = 'g1' onClick = {this.props.onClick}>{this.state.pieceArray[62]}</td>
-              <td id = 'h1' onClick = {this.props.onClick}>{this.state.pieceArray[63]}</td>
+              <td id = 'a1' className = {this.props.chooseClass('a1')} onClick = {this.props.onClick}>{this.state.pieceArray[56]}</td>
+              <td id = 'b1' className = {this.props.chooseClass('b1')} onClick = {this.props.onClick}>{this.state.pieceArray[57]}</td>
+              <td id = 'c1' className = {this.props.chooseClass('c1')} onClick = {this.props.onClick}>{this.state.pieceArray[58]}</td>
+              <td id = 'd1' className = {this.props.chooseClass('d1')} onClick = {this.props.onClick}>{this.state.pieceArray[59]}</td>
+              <td id = 'e1' className = {this.props.chooseClass('e1')} onClick = {this.props.onClick}>{this.state.pieceArray[60]}</td>
+              <td id = 'f1' className = {this.props.chooseClass('f1')} onClick = {this.props.onClick}>{this.state.pieceArray[61]}</td>
+              <td id = 'g1' className = {this.props.chooseClass('g1')} onClick = {this.props.onClick}>{this.state.pieceArray[62]}</td>
+              <td id = 'h1' className = {this.props.chooseClass('h1')} onClick = {this.props.onClick}>{this.state.pieceArray[63]}</td>
             </tr>
             </tbody>
           </table>
