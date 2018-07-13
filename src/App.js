54ee31d2b843d80bdedc7f031a70ff23b24ac8d5
sam1463 <samuel.doyle@alumni.duke.edu>
2018-07-13 00:36:46 -0400

    change jsx to js files, some code cleanliness changes to app.js and promote.js

--- src/App.js
+++ src/App.js
@@ -1,270 +1,309 @@
 import React, { Component } from 'react';
 import './App.css';
 import Chess from 'chess.js'
-import Board from './board.jsx'
-import Captured from './captured.jsx';
-import Promote from './promote.jsx'
+import Board from './board.js'
+import Captured from './captured.js';
+import Promote from './promote.js'
 
-//this function takes the possible moves array and strips the elements to only contain a coordinate
+// this function takes the possible moves array and strips the elements to only contain a coordinate
 const returnSquare = (array, target) => {
-  let returnArray =[];
-  array.forEach((square) =>{
-    let temp = "";
-    let newString = ""
-    //if the last character denotes check or checkmate, cut it off
-    if(square[square.length-1] === '#' || square[square.length-1] === '+'){
-      newString = square.slice(0,-1)
-    }
-    else{
-      newString = square;
-    }
-    //handle pawn promotions
-    if(newString.includes('=')){
-      temp = newString.substr(newString.length-4, 2);
+  let returnArray = [];
+  array.forEach((square) => {
+    let temp = '';
 
+    // if the last character denotes check or checkmate, cut it off
+    const inCheckOrCheckmate = square[square.length - 1] === '#' || square[square.length-1] === '+';
+    let newString = inCheckOrCheckmate ? square.slice(0, -1) : square
+
+    if (newString.includes('=')) {
+      // handle pawn promotions
+      temp = newString.substr(newString.length - 4, 2);
     }
-    //handle castling
-    else if(newString === 'O-O'){
-      if(target === 'e1'){
-        temp = "g1"
-      }
-      else{
-        temp = "g8"
-      }
+
+    else if (newString === 'O-O') {
+      // handle castling
+      temp = (target === 'e1') ?'g1' : 'g8';
     }
-    else if(newString === 'O-O-O'){
-      if(target === 'e1'){
-        temp = "c1"
-      }
-      else{
-        temp = "c8"
-      }
+
+    else if (newString === 'O-O-O') {
+      temp = (target === 'e1') ? 'c1' : 'c8';
     }
-    else{
+
+    else {
       temp = newString.substr(newString.length - 2)
     }
 
     returnArray.push(temp)
-  })
+  });
   return returnArray;
 }
 
 
-
 class App extends Component {
-  constructor(){
+  constructor() {
     super();
-    this.state = {target: "", fen: 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1',
-      gameOver: false, gameOverMessage: "", choosePromo:false, promoLocation: ""}
+    this.state = {
+      target: '',
+      fen: 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1',
+      gameOver: false,
+      gameOverMessage: '',
+      choosePromo:false,
+      promoLocation: '',
+    };
+
     this.chess = new Chess();
-    this.clickSquare = this.clickSquare.bind(this);
-    this.chooseTarget = this.chooseTarget.bind(this);
-    this.computerMove = this.computerMove.bind(this);
-    this.promote = this.promote.bind(this);
-    this.move = this.move.bind(this);
-    this.calculateBestMove = this.calculateBestMove.bind(this);
-    this.evaluateScore = this.evaluateScore.bind(this);
-    this.possibleMoves =[];
-    this.firstClick = false;
+    this.possibleMoves = [];
+    this.hasMadeFirstClick = false;
     this.choosing = false;
     this.turn = 'player';
     this.location = '';
-
-
-
   }
-  componentDidMount(){
-    //load in the starting position from state
-    this.chess.load(this.state.fen)
+
+  componentDidMount() {
+    // load in the starting position from state
+    this.chess.load(this.state.fen);
   }
-  componentDidUpdate(){
-    if(this.state.gameOver){
-      setTimeout(()=>alert(this.state.gameOverMessage),250);
+
+  componentDidUpdate() {
+    if (this.state.gameOver) {
+      setTimeout(
+        () => alert(this.state.gameOverMessage),
+        250,
+      );
     }
-    if(!this.state.gameOver){
-      //if the game is in checkmate, alert the player
-      if(this.chess.in_checkmate()){
-        //if the player just moved, the comp must be in checkmate
-        if(this.turn === 'computer'){
-          this.setState({gameOver: true, gameOverMessage: "Checkmate! You Win."})
-        }
-        else{
-            this.setState({gameOver: true, gameOverMessage: "Checkmate! You Lose."})
-        }
-      }
-      if(this.chess.in_stalemate()){
-        this.setState({gameOver: true, gameOverMessage: "Stalemate! Game is a draw."})
-      }
 
-      if(this.turn === 'computer'){
+    if (!this.state.gameOver) {
+      // if the game is in checkmate, alert the player
+      if (this.chess.in_checkmate()) {
+        // if the player just moved, the comp must be in checkmate
+        this.setState({
+          gameOver: true,
+          gameOverMessage: `'Checkmate! You ${this.turn === 'computer' ? 'Win' : 'Lose'}.`,
+        });
+      } else if (this.chess.in_stalemate()) {
+        this.setState({
+          gameOver: true,
+          gameOverMessage: 'Stalemate! Game is a draw.',
+        });
+      } else if (this.turn === 'computer') {
         this.computerMove();
       }
     }
   }
-  chooseTarget(targeted){
-  //if the target is equal to the clicked on square, outline in red
-  if(targeted === this.state.target){
-    return 'targeted'
+
+  chooseTarget = (targeted) => {
+  // if the target is equal to the clicked on square, outline in red
+  if (targeted === this.state.target) {
+    return 'targeted';
   }
-  else{
-    //if its not the clicked on square, see if its a possible move square
-    for(let i=0; i < this.possibleMoves.length; i++){
-      //if possible move, turn yellow.
-      if(targeted === this.possibleMoves[i].substr(this.possibleMoves[i].length -2)){
-        // console.log(this.possibleMoves[i])
-        return 'possibleMove'
-      }
+
+  // if its not the clicked on square, see if its a possible move square
+  for (let i = 0; i < this.possibleMoves.length; i++) {
+    // if possible move, turn yellow.
+    if (targeted === this.possibleMoves[i].substr(this.possibleMoves[i].length - 2)) {
+      return 'possibleMove';
     }
-    //if it has nothing special about it, stay normal
-    return 'normal'
   }
+  // if it has nothing special about it, stay normal
+  return 'normal';
 }
-  clickSquare(e){
-    if(this.turn === "player" && !this.choosing){
-      //first click,
-      if(!this.firstClick){
-        //find all the possible moves, just the coordinates
+  clickSquare = (e) => {
+    if (this.turn === 'player' && !this.choosing) {
+      if (!this.hasMadeFirstClick) { // first click
+        // find all the possible moves, just the coordinates
         this.possibleMoves = returnSquare(this.chess.moves({square: e.target.id}), e.target.id);
-        //if the piece can move
-        if(this.possibleMoves.length > 0){
-          this.firstClick = true;
-          //console.log(this.possibleMoves);
-          //set the clicked on square to state.target
+        // if the piece can move
+        if (this.possibleMoves.length > 0) {
+          this.hasMadeFirstClick = true;
+          // set the clicked on square to state.target
           this.setState({target: e.target.id})
         }
-      }
-        //second click
-      else{
-        //setup another chess version with the same fen to check if the move causes a promotion
+      } else { // second click
+        // setup another chess version with the same fen to check if the move causes a promotion
         let checker = new Chess(this.state.fen)
 
-        //if the click is on a possible move square, move the first click piece to second click location
+        // if the click is on a possible move square, move the first click piece to second click location
         this.location = e.target.id;
-        //cycle through the possible moves and see if the clicked square is a possible move
-        for(let i=0; i < this.possibleMoves.length; i++){
-          if(this.location === this.possibleMoves[i].substr(this.possibleMoves[i].length -2)){
-            //if the move causes a promotion, we need to get the selected promo
-            if(checker.move({from: this.state.target, to: this.location, promotion: 'q'}).flags.includes('p')){
-              //set choosePromo to true and store the location of the target in a new state variable so it isn't cleared out
-              this.setState({choosePromo: true, promoLocation: this.state.target});
+        // cycle through the possible moves and see if the clicked square is a possible move
+        for(let i = 0; i < this.possibleMoves.length; i++) {
+          if (this.location === this.possibleMoves[i].substr(this.possibleMoves[i].length - 2)) {
+            // if the move causes a promotion, we need to get the selected promo
+            if (checker.move({from: this.state.target, to: this.location, promotion: 'q'}).flags.includes('p')) {
+              // set choosePromo to true and store the location of the target in a new state variable so it isn't cleared out
+              this.setState({
+                choosePromo: true,
+                promoLocation: this.state.target,
+              });
               this.choosing = true;
-              break;  //jump out of for loop because it has 1 possible move for each of promotion options
-            }
-            //if there is no promotion, just move the piece with promo: 'q' since it won't do anything
-            else{
+              break;  // jump out of for loop because it has 1 possible move for each of promotion options
+            } else {
+              // if there is no promotion, just move the piece with promo: 'q' since it won't do anything
               this.move(this.state.target,this.location, 'q');
             }
           }
         }
-        //reset possible move squares and target to unhighlights squares
-        this.possibleMoves =[];
-        this.firstClick = false;
-        this.setState({target:""})
-
-
-
+        // reset possible move squares and target to unhighlights squares
+        this.possibleMoves = [];
+        this.hasMadeFirstClick = false;
+        this.setState({ target: '' });
       }
     }
   }
-  move(target,location,promo){
-    //move the chess piece, change turn, and set new state for fen, clear the rest.
-    this.chess.move({from: target, to:location, promotion:promo});
+  move = (target,location,promo) => {
+    // move the chess piece, change turn, and set new state for fen, clear the rest.
+    this.chess.move({
+      from: target,
+      to:location,
+      promotion:promo,
+    });
     this.turn = 'computer';
-    this.setState({fen: this.chess.fen(), target:"", promoLocation:'', choosePromo: false})
+    this.setState({
+      fen: this.chess.fen(),
+      target:'',
+      promoLocation:'',
+      choosePromo: false,
+    });
   }
-  evaluateScore(fen){
-    //just count up the pieces and multiple by point value;
-    let whitePawns, blackPawns, whiteQueens, blackQueens, whiteRooks, whiteKnights,
-    whiteBishops, blackRooks, blackKnights, blackBishops;
 
-    whitePawns = blackPawns = whiteQueens = blackQueens = whiteRooks = whiteKnights =
-    whiteBishops = blackRooks = blackKnights = blackBishops = 0;
+  evaluateScore = (fen) => {
     let game = new Chess(fen);
-    if(game.in_checkmate()){
-      return -10000;
+    if (game.in_checkmate()) {
+      return 10000;
     }
 
-    for(let x of fen){
-      if(x === 'Q'){whiteQueens ++;}
-      if(x === 'R'){whiteRooks ++;}
-      if(x === 'N'){whiteKnights ++;}
-      if(x === 'B'){whiteBishops ++;}
-      if(x === 'P'){whitePawns ++;}
-      if(x === 'q'){blackQueens ++;}
-      if(x === 'r'){blackRooks ++;}
-      if(x === 'n'){blackKnights ++;}
-      if(x === 'b'){blackBishops ++;}
-      if(x === 'p'){blackPawns ++;}
-      if(x === ' '){break;}
-    }
-    return (((whiteQueens * 9)+(whiteRooks * 5)+(whiteKnights * 3)+ (whiteBishops * 3)+(whitePawns)) -
-    ((blackQueens * 9)+(blackRooks * 5) + (blackKnights * 3)+(blackBishops * 3)+(blackPawns)))
+    let score = 0;
 
+    // the fen might have an extra 'b' for black after the list of codes, e.g.
+    // rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq e3 0 1, so we strip out only the codes to count
+    const boardPieceCodes = fen.split(' ')[0];
+
+    // just loop through all the pieces and update the point value;
+    for (let pieceCode of boardPieceCodes) {
+      switch (pieceCode) {
+        case ('Q'): {
+          score -= 9;
+          break;
+        }
+
+        case ('R'): {
+          score -= 5;
+          break;
+        };
+
+        case ('N'): {
+          score -= 3;
+          break;
+        };
+
+        case ('B'): {
+          score -= 3;
+          break;
+        };
+
+        case ('P'): {
+          score --;
+          break;
+        };
+
+        case ('q'): {
+          score += 9;
+          break;
+        }
+
+        case ('r'): {
+          score += 5;
+          break;
+        };
+
+        case ('n'): {
+          score += 3;
+          break;
+        };
+
+        case ('b'): {
+          score += 3;
+          break;
+        };
+
+        case ('p'): {
+          score ++;
+          break;
+        };
+
+        default: {
+          break;
+        }
+      };
+    }
+    return score;
   }
-  calculateBestMove(){
-    //get all the moves
+
+  calculateBestMove = () => {
+    // get all the moves
     let possibleMoves = this.chess.moves();
-    let bestMove = null;
+    // keep a list of all equally best moves to choose from
+    let bestMoves = [];
     let bestValue = -100;
-    //calc current score
-    let currentScore = -this.evaluateScore(this.state.fen)
-    //for each possible move, make the move and calculate the score
-    possibleMoves.forEach((move)=>{
+    // calc current score
+    let currentScore = this.evaluateScore(this.state.fen);
+    // for each possible move, make the move and calculate the score
+    possibleMoves.forEach((move) => {
       this.chess.move(move);
-      let value = -this.evaluateScore(this.chess.fen());
-      this.chess.undo()
-      //if the value of the move is better than the current best move, update
-      if(value > bestValue){
+      let value = this.evaluateScore(this.chess.fen());
+      this.chess.undo();
+      // if the value of the move is better than or equal the current best move, update
+      if (value > bestValue) {
         bestValue = value;
-        bestMove = move;
+        bestMoves = [move];
+      } else if (value === bestValue) {
+        bestMoves.push(move);
       }
-    })
-    //if the best value is the current score, make a random move for now (stops comp from making first move every time)
-    if(bestValue === currentScore){
-      return possibleMoves[Math.floor(Math.random() * possibleMoves.length)]
-    }
-    return bestMove;
+    });
 
-
+    // pick a random move from the set of best moves (stops comp from making first move every time)
+    return bestMoves[Math.floor(Math.random() * bestMoves.length)];
   }
-  computerMove(){
-    if(!this.chess.in_checkmate() && !this.chess.in_stalemate()){
-      setTimeout(()=>{
+
+  computerMove = () => {
+    if (!this.chess.in_checkmate() && !this.chess.in_stalemate()) {
+      setTimeout(() => {
         let moves = this.chess.moves();
         //let random = Math.floor(Math.random() * moves.length);
         let computerMove = this.calculateBestMove();
         //Auto promote to Queen for the time being
-        if(computerMove.includes('=')){
+        if (computerMove.includes('=')) {
           computerMove = computerMove.replace(/N/, 'Q').replace(/B/, 'Q').replace(/R/,'Q');
         }
         this.chess.move(computerMove);
-        this.turn = "player";
+        this.turn = 'player';
         this.setState({fen: this.chess.fen()});
       },250);
     }
   }
 
-  promote(e){
-    //get the promotion and move the piece
+  promote = (e) => {
+    // get the promotion and move the piece
     let choice = e.target.id;
-    this.move(this.state.promoLocation,this.location, choice)
-    //set choosing to false so the user can click on pieces again
+    this.move(this.state.promoLocation, this.location, choice);
+    // set choosing to false so the user can click on pieces again
     this.choosing = false;
-
   }
+
   render() {
-    let promo;
-    if(this.state.choosePromo){
-      promo = <Promote onClick = {this.promote}/>
-    }
+    const {
+      choosePromo,
+      fen,
+    } = this.state;
+
     return (
       <div>
-        {promo}
-        <Captured fen = {this.state.fen} color = 'white'/>
-        <Board onClick = {this.clickSquare} chooseClass = {this.chooseTarget} fen = {this.state.fen}/>
-        <Captured fen = {this.state.fen} color = 'black'/>
+        {choosePromo && (
+          <Promote onClick={this.promote}/>
+        )}
+        <Captured fen={fen} color ="white"/>
+        <Board onClick={this.clickSquare} chooseClass={this.chooseTarget} fen={fen}/>
+        <Captured fen={fen} color="black"/>
       </div>
-
     );
   }
 }
