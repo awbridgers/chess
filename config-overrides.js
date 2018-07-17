3c2c20d3279782f6ca4e0e24b3609d55744227b3
awbridgers <awbridgers@users.noreply.github.com>
2018-07-16 20:15:54 -0400

    update to fix build issue

--- config-overrides.js
+++ config-overrides.js
@@ -1,7 +1,10 @@
+
+
 module.exports = function override(config, env) {
     config.module.rules.push({
         test: /\.worker\.js$/,
         use: { loader: 'worker-loader' }
       })
+      config.node = {readline: 'empty', fs: 'empty'}
     return config;
 }
